5fc450e745a163c04f03f459d5f0a0f0b91e261d
mecarito <pieritomecar@gmail.com>
2022-04-16 01:17:30 +0300

    getting media stream from the user

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 
 export default function Main() {
-    return <div>main screen</div>
+  useLayoutEffect(() => {
+    async function fetchMedia() {
+      const constraints = { audio: true, video: { width: 1000, height: 600 } };
+      const mediaStream = await navigator.mediaDevices.getUserMedia(
+        constraints
+      );
+      const video = document.querySelector('video');
+      if (video) {
+        video.muted = true;
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = () => video?.play();
+      }
+    }
+    fetchMedia();
+  });
+
+
+  return (
+    <div className="p-8">
+      <h1 className=' text-dark-blue text-2xl font-normal-text font-semibold '>Overview of new real estate proposals</h1>
+      <video className="mt-6 rounded-2xl " width={1000} height={600}>
+        Sorry, your browser does not support video
+      </video>
+    </div>
+  );
 }

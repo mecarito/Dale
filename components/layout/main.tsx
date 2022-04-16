b11261915477dc797162cda76f056a152faaeef1
mecarito <pieritomecar@gmail.com>
2022-04-16 15:45:11 +0300

    minor change

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -19,15 +19,17 @@ export default function Main() {
   });
 
   return (
-    <div className="p-8 relative">
+    <div className="p-8">
       <h1 className=" text-dark-blue text-2xl font-normal-text font-semibold ">
         Overview of new real estate proposals
       </h1>
-      <video className="mt-6 rounded-2xl " width={1000} height={600}>
-        Sorry, your browser does not support video
-      </video>
-      <div className='mt-10 absolute'>
-        <ControlsView />
+      <div className='relative'>
+        <video className="mt-6 rounded-2xl " width={1000} height={600}>
+          Sorry, your browser does not support video
+        </video>
+        <div className="absolute top-0">
+          <ControlsView />
+        </div>
       </div>
     </div>
   );
